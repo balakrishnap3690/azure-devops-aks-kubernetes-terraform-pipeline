d833ac0a047821da399d77198d531f2eb102172e
balakrishnap3690 <117189598+balakrishnap3690@users.noreply.github.com>
2022-11-04 07:50:37 +0000

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   default_node_pool {
     name       = "systempool"
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_D2dS_v5"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     availability_zones   = [1, 2, 3]
     enable_auto_scaling  = true
